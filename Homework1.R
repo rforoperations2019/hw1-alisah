c06beb7d5f103371faf3159a0876faef95d50e01
alisahartle <54589015+alisahartle@users.noreply.github.com>
2019-09-07 00:32:59 -0400

    Update Homework1.R

--- Homework1.R
+++ Homework1.R
@@ -1,15 +1,3 @@
-# Directions: 
-# * Create three (3) different kinds of plots/figures 
-# * Use DT to create one (1) data table
-# * Include at least three (3) different types of inputs
-# * One (1) functioning downloadButton() 
-# * One (1) observer in the server
-# * Inputs must use reactivity in a logical manner with all outputs displayed to users
-
-
-# scatterplot of tipping by trip distance, trip duration, fare_amount (radio buttons)
-# scatter plot of dates on the x, tip amount on y. color points by number of passengers (dateRangeinput?)
-# box plots of number of passengers, vary plot by fare, distance, tip (selecinput)
 
 
 #Data retrieved from https://catalog.data.gov/dataset/2019-green-taxi-trip-data
